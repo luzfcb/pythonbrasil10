82063d8cb0c169d8fa8ae23d3c41fe5a17f6bbaa
Fabio Caritas Barrionuevo da Luz <bnafta@gmail.com>
2014-07-03 11:47:17 -0300

    added first approach

--- pythonbrasil8/core/context_processors.py
+++ pythonbrasil8/core/context_processors.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from mittun.sponsors.models import Sponsor
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 def diamond_sponsors(request):
@@ -12,3 +14,10 @@ def sponsors(request):
     sponsors = Sponsor.objects.select_related('category__priority')
     sponsors = sponsors.order_by('category__priority')
     return {'sponsors': sponsors}
+
+
+def conference_edition_number(request):
+    if not hasattr(settings, 'CONFERENCE_EDITION_NUMBER'):
+        raise ImproperlyConfigured(
+            'Please, define variable CONFERENCE_EDITION_NUMBER in your settings file')
+    return {'conference_edition_number': settings.CONFERENCE_EDITION_NUMBER}
